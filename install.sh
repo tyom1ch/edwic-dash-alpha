b64281a6e8bbe256b37661c9356bfeb969d8c0e5
Tyom1ch <tyomchbb@gmail.com>
2025-07-01 13:28:29 +0300

    upd bash

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ docker rm $CONTAINER_NAME 2>/dev/null || true
 echo "Starting container..."
 docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE
 
-IP=$(hostname -I | awk '{print $1}')
+# Визначаємо IP через ip route
+IP=$(ip route get 1 | awk '{print $7; exit}')
 URL="http://$IP:$PORT"
 
-echo "Йоу, заходь сюди: $URL"
+echo "Заходь сюди: $URL"
 echo "Перевірка доступності..."
 
-curl --fail $URL && echo "Апка відповідає!" || echo "Щось не так, апка не відповідає."
-
+curl --fail $URL && echo "Все гуд)" || echo "Щось не так..."
